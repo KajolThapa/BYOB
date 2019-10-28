ef3969c8bfcfbb726b8379968fe3eea89c90f9ca
Kajol Thapa <kajoolthapa@gmail.com>
2019-10-28 13:46:27 -0400

    Implemented useState() hook in App component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: "Momo", age: 4},
       {name: "Mila", age: 1},
-    ]
-  }
-  switchNameHandler = () => {
-    this.setState({
+    ],
+  });
+  const [otherState, setOtherState] = useState("some other value")
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name : "Momo Hauke", age: 5},
         {name: "Mila", age: 2},
-      ]
+      ],
     })
   }
-  render() {
     return (
       <div className="App">
         <h1></h1>
-        <button onClick={this.switchNameHandler}> Switch Name </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Hobbies: Playing with laser </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick={switchNameHandler}> Switch Name </button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}> Hobbies: Playing with laser </Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
       </div>
     );
-  }
 }
 
 export default App;
+
+
+
